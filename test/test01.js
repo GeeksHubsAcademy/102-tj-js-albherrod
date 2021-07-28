860dab2a0d75516f1dbf5ee6863694c176be9940
ahernandezr05 <datw.alberto@gmail.com>
2021-07-28 18:27:57 +0200

    FINAL Tiempo: 50 minutos. Intentos: 1

--- test/test01.js
+++ test/test01.js
@@ -1,5 +1,27 @@
 function numPrimo(input) {
- return null;
+    //Voy a suponer que solo se introducen numeros positivos y no decimales
+    resultado = new Array();
+    if(input == 1){
+        resultado[0]=-1;
+        return resultado;
+    }else{
+        for(j=2; j<input; j++){
+            if(esPrimo(j)){
+                resultado.push(j);
+            }
+        }
+        resultado.push(input);
+    }
+    console.log(resultado)
+    return resultado;
 }
-
+function esPrimo(numero){
+    for(i=2; i<numero; i++){
+        if(numero%i === 0){
+            return false;
+        }
+    }
+    return numero !== 1;
+}
+numPrimo(13);
 module.exports = numPrimo;

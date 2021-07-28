fbc0ad7adf9434143621ceb4f7b0cdcc372441b2
ahernandezr05 <datw.alberto@gmail.com>
2021-07-28 19:02:47 +0200

    FINAL Tiempo >50 Intentos 5

--- test/test01.js
+++ test/test01.js
@@ -2,8 +2,7 @@ function numPrimo(input) {
     //Voy a suponer que solo se introducen numeros positivos y no decimales
     resultado = new Array();
     if(input == 1){
-        resultado[0]=-1;
-        return resultado;
+        return -1;
     }else{
         for(j=2; j<input; j++){
             if(esPrimo(j)){
